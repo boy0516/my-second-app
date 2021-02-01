c49835162ced17210b417ffe44cb5835bb1d2620
choi <boy0516@naver.com>
2021-02-01 15:39:49 +0900

    update

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ const port = process.env.PORT || 3000;
 
 // http://127.0.0.1:3000/
 app.get('/', (req, res) => res.send('Hello, Nodejs!'))
+app.get('/welcom',(req, res)=>res.send('Hi,ther.welcome to the nodejs'))
 app.listen(port, () => console.log('Example app listen on port 3000'))
