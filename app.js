844420b58a738f166145f4d395d5e9bc28e86c3f
choi <boy0516@naver.com>
2021-02-01 16:43:57 +0900

    up

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3000;
 
 // http://127.0.0.1:3000/
 app.get('/', (req, res) => res.send('Hello, Nodejs!'))
-app.get('/welcome',(req, res)=>res.send('Hi,ther.welcome to the nodejs'))
+// http://127.0.0.1:3000/welcom?name=choi
+app.get('/welcome',(req, res)=>{
+    var user_name = req.param('name')
+    res.send('Hi,ther.welcome to the nodejs. [' + user_name +')')
+})
 app.get('/check',(req, res)=>res.send('Example app listen on port 3000'))
 app.listen(port, () => console.log('Example app listen on port 3000'))
